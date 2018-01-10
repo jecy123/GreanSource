05d4262a6c839651ccc422e873036b908ee5c153
星空 <cloudy@macbook-air.local>
2018-01-10 15:03:53 +0800

    Initial Commit

---
+++ GreanSourceManeger/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GreanSourceManeger
+//
+//  Created by 星空 on 2018/1/10.
+//  Copyright © 2018年 星空. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
