060cb315d5f8a7604b836e2d5dc6fe2a3f055e0b
星空 <cloudy@macbook-air.local>
2018-01-11 16:22:42 +0800

    改进

--- GreanSourceManeger/ViewController.swift
+++ GreanSourceManeger/ViewController.swift
@@ -10,9 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var btnLogin:UIButton!
+    @IBOutlet var btnRegister:UIButton!
+    @IBOutlet var btnRefind:UIButton!
+    @IBOutlet var btnRememberPass:UIButton!
+    @IBOutlet var btnAutoLogin:UIButton!
+    
+    func initViews() {
+        //btnLogin.layer.borderWidth = 1
+        btnLogin.layer.backgroundColor = UIColor(red: 25/255, green: 140/255, blue: 58/255, alpha: 1).cgColor //  UIColor.green.cgColor
+        btnLogin.layer.cornerRadius = 5
+        
+        //btnLogin.layer.masksToBounds = true
+        
+        btnLogin.addTarget(self, action: #selector(onClick(_:)), for: .touchUpInside)
+        
+        btnRegister.addTarget(self, action: #selector(onClick(_:)), for: .touchUpInside)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initViews()
+        
+    }
+    
+    @objc func onClick(_ sender : UIButton) {
+        if sender == btnLogin {
+            self.performSegue(withIdentifier: "toMain", sender: self)
+        }else if sender == btnRegister
+        {
+            self.performSegue(withIdentifier: "toRegister", sender: self)
+        }
     }
 
     override func didReceiveMemoryWarning() {
