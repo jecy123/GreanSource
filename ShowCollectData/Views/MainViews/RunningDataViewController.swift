944c9edb9a8b842698dfe5fbfe5c8e730f3baae7
星空 <cloudy@macbook-air.local>
2018-02-24 13:50:23 +0800

    修改

---
+++ ShowCollectData/Views/MainViews/RunningDataViewController.swift
@@ -0,0 +1,27 @@
+//
+//  RunningDataViewController.swift
+//  ShowCollectData
+//
+//  Created by 星空 on 2018/2/23.
+//  Copyright © 2018年 星空. All rights reserved.
+//
+
+import UIKit
+
+//运行数据界面
+class RunningDataViewController: BasePageViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func onMenu(_ sender: UIButton) {
+        print("运行数据界面菜单")
+        self.delegate?.onBack(self)
+    }
+}
